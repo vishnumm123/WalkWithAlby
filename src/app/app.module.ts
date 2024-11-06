039562a441a88403232a7c8efbbe2d85ebf57579
Vishnu MM <mmvishnucalicut@gmail.com>
2024-11-06 23:23:47 +0530

    We are live

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './modules/workbench/layout/components/header/header.component';
+import { MainModule } from './modules/workbench/main/main.module';
+import { HomeComponent } from './modules/workbench/main/components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
